46fecaa9aa5f853202f17420369382f527b7ee6f
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-07 20:45:40 -0700

    basic HTML and CSS completed, retrieved DOM elements.

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // import functions and grab DOM elements
+//import { isGuessCorrect, isGuessLow } from "./cookieUtils";
+const userNumber = document.querySelector("#number-input");
+const userSubmit = document.querySelector("#submit-button");
+const triesRemaining = document.querySelector("#guesses-remaining");
+const highOrLow= document.querySelector("#guess-was");
 
 // initialize state
 
