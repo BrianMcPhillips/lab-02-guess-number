17865dac4f90f9acd595b80f50567d28fe5f2444
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-07 23:49:00 -0700

    some javascript, not close to being complete.

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const userNumber = document.querySelector("#number-input");
 const userSubmit = document.querySelector("#submit-button");
 const triesRemaining = document.querySelector("#guesses-remaining");
 const highOrLow= document.querySelector("#guess-was");
+const resultDisplay = document.querySelector("#result-display");
 
 // initialize state
-
+let guessesRemaining = 4;
+const computerNumber = Math.ceil(Math.random() * 20);
 // set event listeners to update state and DOM
+userSubmit.addEventListener('click', () => {
+    if (triesRemaining > 0) {
+    const userGuess = Number()
+    }
+});
